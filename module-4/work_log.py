53774446cd44d71d168516a44b66ae4cccd60ca1
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2023-10-16 23:04:49 +0100

    examples from students lecture

--- module-4/work_log.py
+++ module-4/work_log.py
@@ -5,3 +5,35 @@ m = '13/10/2023'
 m_1 = m.split('/')
 print(m_1)
 
+cars = ['VW', 'Tesla', 'BMW', 'Toyota']
+print(cars)
+a = ', '.join(cars)
+print(a)
+a = ' | '.join(cars)
+print(a)
+print(type(a))
+
+print(cars)
+print(cars[0])
+print(cars[3])
+print(cars[-1])
+print(cars.index('Tesla'))
+
+test = [ 5, 12.5, True]
+
+all_info = [cars, 'test', test]
+print(all_info)
+print(all_info[0])
+print(all_info[1])
+print(all_info[2])
+print(all_info[0][3])
+
+cars1 = ['VW', 'Tesla', 'BMW', 'Toyota']
+cars1[0] = 'Ford'
+print(cars1)
+
+cars2 = ['VW', 'Tesla', 'BMW', 'Toyota']
+cars2.append('Opel')
+print(cars2)
+cars2.insert(3, 'AMG')
+print(cars2)
