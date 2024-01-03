82eac616a023aba11a81567b7ed6df8e3a7d6aa3
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2024-01-03 23:34:41 +0000

    Solution for exercise 3

--- Module_8/ex_3.py
+++ Module_8/ex_3.py
@@ -6,9 +6,16 @@ from datetime import datetime
 
 
 def get_str_date(date):
-    s = datetime.fromisoformat(date[:-1])
-    print(s)
-    return
+    # Перетворюємо вхідну дату з рядка у об'єкт datetime
+    date_object = datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%fZ')
+    print(date_object)
+    # форматуємо дату у бажаний вигляд
+    formated_date = date_object.strftime('%A %d %B %Y')
+    print(formated_date)
+    return  formated_date
 
 
 print(get_str_date('2021-05-27 17:08:34.149Z'))
+
+# для вирішення ми скористались двума головними методами strptime and strftime are both functions used for formatting
+# and parsing dates and times in Python, but they serve different purposes
