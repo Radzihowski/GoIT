0b95d8e95736e4c8b187732ed6a0cebecd65b90e
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2024-01-18 21:22:38 +0000

    exercise 4

--- module_9/ex_4_discount_price.py
+++ module_9/ex_4_discount_price.py
@@ -22,13 +22,7 @@
 # 95.0
 
 def discount_price(discount):
-    price = 100
-    DISCOUNT = {
-        cost_15 = discount_price(0.15),
-        cost_10 = discount_price(0.10),
-        cost_05 = discount_price(0.05)
-
-
-print(cost_15(price))
-print(cost_10(price))
-print(cost_05(price))
+    def calculate_price(price):
+        print(price * ( 1 - discount))
+        return price * ( 1 - discount)
+    return calculate_price
