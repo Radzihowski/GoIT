cb52c5fa0c24b7e3d55c8bf4705f6118767c50c0
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2024-02-09 22:36:06 +0000

    Solution for task 11

---
+++ module_5/practice/ex_11.py
@@ -0,0 +1,16 @@
+# To consolidate the material, write a find_all_words function that searches for a word match in the text.
+# The function returns a list of all occurrences of the word in the word parameter in the text in the form of any
+# spelling, that is, for example, there are possible spellings of the word "Python" as pYthoN, pythOn, PYTHOn, etc.
+# the main thing is that the word order is preserved, the case does not matter.
+#
+# Hint: the functions of the re module also take key parameter flags and we can determine case insensitivity by setting
+# it to re.IGNORECASE.
+
+import re
+
+
+def find_all_words(text, word):
+    word_pattern = "^\S*\s|\s\S*\s|\s\S*$"
+    result = re.findall(word_pattern, text)
+    print(result)
+    return result
