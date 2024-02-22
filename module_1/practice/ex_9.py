d306899edadddfc24a15555ee9ebe96f2d86400c
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2024-02-22 21:41:16 +0000

    Some code

--- module_1/practice/ex_9.py
+++ module_1/practice/ex_9.py
@@ -21,4 +21,4 @@
 length = "2.75"
 width = "1.75"
 area = float(length) * float(width)
-show = f"With width {length} and length {width} of the room, its area is equal to {area}"
+show = f"With width {width} and length {length} of the room, its area is equal to {area}"
