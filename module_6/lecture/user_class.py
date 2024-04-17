d42a06e688cd129a52dd490d2f7343ac830f9938
DESKTOP-JPEJ3R0\ARadz <a.radzihovskiy@gmail.com>
2024-04-17 22:40:18 +0100

    Home work for week

---
+++ module_6/lecture/user_class.py
@@ -0,0 +1,14 @@
+class User:
+    name = "Anonymous"
+    age = 15
+
+user1 = User()
+print(user1.name)
+print(user1.age)
+
+user2 = User()
+user2.name = "John"
+user2.age = 90
+
+print(user2.name)
+print(user2.age)
