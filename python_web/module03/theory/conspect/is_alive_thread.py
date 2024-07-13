10f8a6403d0f250eb37bdfc8c698fff2cc995c4b
Oleksandr Radzikhovskyi <A.Radzihovskiy@gmail.com>
2024-07-13 12:27:15 +0100

    added pre-commit functionality

--- python_web/module03/theory/conspect/is_alive_thread.py
+++ python_web/module03/theory/conspect/is_alive_thread.py
@@ -1,6 +1,9 @@
+# Ви також можете перевірити — чи виконується потік, викликавши метод is_alive:
+
 from threading import Thread
 from time import sleep
 import logging
+
 
 class UsefulClass:
     def __init__(self, second_num):
@@ -8,10 +11,11 @@ class UsefulClass:
 
     def __call__(self):
         sleep(self.delay)
-        logging.debug('Wake up!')
+        logging.debug("Wake up!")
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
     t2 = UsefulClass(1)
     thread = Thread(target=t2)
     thread_locking = Thread(target=t2)
@@ -22,4 +26,12 @@ if __name__ == '__main__':
     thread.join()
     thread_locking.join()
     print(thread.is_alive(), thread_locking.is_alive())
-    print('After all...')
+    print("After all...")
+
+# True False
+# Thread - 2 Wake up!
+# Thread - 1 Wake up!
+# False False
+# After all...
+
+# Це може бути корисним, якщо ви хочете перевіряти стан потоку самостійно і не блокувати застосунок в очікуванні завершення.
