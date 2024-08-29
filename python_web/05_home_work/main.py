c37e6f1f34ad1fe7694231ca5e6e4be9e2874560
Radzihowski <a.radzihovskiy@gmail.com>
2024-08-29 19:41:45 +0100

    Some theory for week 5 and home work for week 5

--- python_web/05_home_work/main.py
+++ python_web/05_home_work/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import platform
 import sys
 import time
@@ -15,6 +16,7 @@ async def get_data(session, day):
 
 async def main():
     param = sys.argv
+    currency = ["EUR", "USD"]
     if len(param) < 2:
         print("Enter number of days")
         return
@@ -23,8 +25,12 @@ async def main():
         if day_count > 10 and day_count < 1:
             print("Parameters should be between 1 and 10")
             return
-
+        if len(param) >= 3:
+            user_currency = param[2:]
+            currency.extend(user_currency)
+            currency = [c.upper() for c in currency]
     dates = []
+    print(currency)
 
     for i in range(1, day_count + 1):
         date_stamp = datetime.now() - timedelta(days=i)
@@ -35,13 +41,30 @@ async def main():
     async with aiohttp.ClientSession() as session:
         coroutines = [get_data(session, day) for day in dates]
         result = await asyncio.gather(*coroutines)
-        print(result)
+
+    return result, currency
+def parse_exchange(exchanges:list[dict], currency:list):
+    result = []
+
+    for i in exchanges:
+        date = i["date"]
+        rate = {date: {}}
+        for el in i["exchangeRate"]:
+            if el["currency"] in currency:
+                sale = round(el["saleRate"], 1)
+                purchase = round(el["purchaseRate"], 1)
+                rate[date].update({el["currency"]: {"sale": sale, "purchase": purchase}})
+        result.append(rate)
+    print(result)
+    return result
 
 if __name__ == "__main__":
     start = time.time()
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
-    print(r)
+    r, currency = asyncio.run(main())
+    parsed_data = parse_exchange(r, currency)
+    with open("exchange.json", "w", encoding="utf-8") as f:
+        json.dump(parsed_data, f, ensure_ascii=False, indent=4)
     stop = time.time()
     print(f"Execution time = {stop - start}")
