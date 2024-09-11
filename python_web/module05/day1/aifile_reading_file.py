4e8cf267672ad81acca825c51b5fffbe16a1471d
Radzihowski <a.radzihovskiy@gmail.com>
2024-09-11 20:01:07 +0100

    theoretical task from week 5

---
+++ python_web/module05/day1/aifile_reading_file.py
@@ -0,0 +1,9 @@
+import asyncio
+from aifile import async_open
+
+async def main():
+    async with async_open("hello.txt", 'r') as afp:
+        print(await afp.read())
+
+if __name__ == '__main__':
+    asyncio.run(main())
