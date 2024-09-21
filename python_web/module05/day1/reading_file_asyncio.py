f1e19ab5c3aa436a78dceb045f9f9408dbf891ba
Radzihowski <a.radzihovskiy@gmail.com>
2024-09-21 21:46:21 +0100

    Coding from week 5

---
+++ python_web/module05/day1/reading_file_asyncio.py
@@ -0,0 +1,12 @@
+# Підхід await afp.read()
+
+import asyncio
+from aiofile import async_open
+
+async  def main():
+    async with async_open("hello.txt", 'r') as afp:
+        print(await afp.read())
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
