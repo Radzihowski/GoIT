d9064eb4f60f24c95e645303bea743d3f0ae9fe1
Radzihowski <a.radzihovskiy@gmail.com>
2024-09-30 20:30:36 +0100

    HW 6

--- python_web/06_home_work/create_tables.py
+++ python_web/06_home_work/create_tables.py
@@ -11,10 +11,31 @@ student_table='''
 groups='''
     CREATE TABLE IF NOT EXISTS groups (
      id SERIAL PRIMARY KEY,
-     group_name text NOT NULL,
+     group_name text NOT NULL
+    );
+'''
+
+teachers='''
+    CREATE TABLE IF NOT EXISTS teachers (
+     id SERIAL PRIMARY KEY,
+     title text NOT NULL,
+     first_name text NOT NULL,
+     second_name text NOT NULL
+    );
+'''
+
+subjects='''
+    CREATE TABLE IF NOT EXISTS subjects (
+     id SERIAL PRIMARY KEY,
+     teacher_id integer NOT NULL,
+     title text NOT NULL,
+     FOREIGN KEY (teacher_id) REFERENCES teachers (id)
     );
 '''
 
 
 if __name__ == "__main__":
     run_query(student_table)
+    run_query(groups)
+    run_query(teachers)
+    run_query(subjects)
--- python_web/06_home_work/fill_db.py
+++ python_web/06_home_work/fill_db.py
@@ -1,7 +1,13 @@
+import numpy as np
+import random
 from faker import Faker
+
 from connect import run_query
 
 fake = Faker()
+SUBJECT_LIST = ["mathematics", "geography", "chemistry", "english", "history"]
+random.shuffle(SUBJECT_LIST)
+
 
 def fill_students(n: int):
     for i in range(1,n):
@@ -17,10 +23,50 @@ def fill_groups(n: int):
     for i in range(1,n+1):
         name_of_the_group = "LA"+str(i)
         print(name_of_the_group)
-        # sql = '''
-        #             INSERT INTO students(LA01,LA02,LA03) VALUES(%s,%s,%s);
-        #         '''
+        sql = '''
+                    INSERT INTO groups(group_name) VALUES(%s);
+                '''
+        run_query(sql, (name_of_the_group,))
+
+
+def teachers(n: int):
+    for i in range(1, n):
+        gender = np.random.choice(["Mr", "Ms"], p=[0.5, 0.5])
+        first_name = fake.first_name_male() if gender == "Mr" else fake.first_name_female()
+        second_name = fake.last_name()
+        print(first_name, second_name)
+        sql = ''' 
+            INSERT INTO teachers(title, first_name,second_name) VALUES(%s,%s,%s);
+        '''
+        run_query(sql, (gender, first_name, second_name))
+
+
+def getteachers_ids():
+    sql = ''' 
+                SELECT id FROM teachers;
+            '''
+    result = run_query(sql)
+    id_list = [item[0] for item in result]
+    return id_list
+
+
+def subjects(subject_list: list):
+    teachers_ids = getteachers_ids()
+    i = 0
+    for subject in subject_list:
+        if i >= len(subject_list)-1:
+            i = 0
+        teacher_id = teachers_ids[i]
+        i+=1
+        print(teacher_id)
+        sql = '''
+            INSERT INTO subjects(title, teacher_id) VALUES(%s,%s);
+        '''
+        run_query(sql, (subject, teacher_id))
 
 if __name__ == "__main__":
     # fill_students(5)
-    fill_groups(3)
+    # fill_groups(3)
+    # teachers(5)
+    # getteachers_ids()
+    subjects(SUBJECT_LIST)
