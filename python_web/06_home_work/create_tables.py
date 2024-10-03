0121f3c4d4232f9fc3ca740a2a1a6c3dba916d6a
Radzihowski <a.radzihovskiy@gmail.com>
2024-10-03 20:41:36 +0100

    HW 6

--- python_web/06_home_work/create_tables.py
+++ python_web/06_home_work/create_tables.py
@@ -4,7 +4,9 @@ student_table='''
     CREATE TABLE IF NOT EXISTS students (
      id SERIAL PRIMARY KEY,
      first_name text NOT NULL,
-     second_name text NOT NULL
+     second_name text NOT NULL,
+     group_id INTEGER NOT NULL,
+     FOREIGN KEY (group_id) REFERENCES groups (id)
     );
 '''
 
@@ -27,15 +29,28 @@ teachers='''
 subjects='''
     CREATE TABLE IF NOT EXISTS subjects (
      id SERIAL PRIMARY KEY,
-     teacher_id integer NOT NULL,
+     teacher_id INTEGER NOT NULL,
      title text NOT NULL,
      FOREIGN KEY (teacher_id) REFERENCES teachers (id)
     );
 '''
 
+marks='''
+    CREATE TABLE IF NOT EXISTS marks (
+     id SERIAL PRIMARY KEY,
+     mark INTEGER NOT NULL,
+     student_id INTEGER NOT NULL,
+     subject_id INTEGER NOT NULL,
+     date DATE NOT NULL,
+     FOREIGN KEY (student_id) REFERENCES students (id),
+     FOREIGN KEY (subject_id) REFERENCES subjects (id)
+    );
+'''
+
 
 if __name__ == "__main__":
-    run_query(student_table)
     run_query(groups)
+    run_query(student_table)
     run_query(teachers)
     run_query(subjects)
+    run_query(marks)
--- python_web/06_home_work/fill_db.py
+++ python_web/06_home_work/fill_db.py
@@ -1,23 +1,45 @@
+import datetime
 import numpy as np
 import random
-from faker import Faker
 
+from faker import Faker
+from itertools import cycle
 from connect import run_query
 
 fake = Faker()
 SUBJECT_LIST = ["mathematics", "geography", "chemistry", "english", "history"]
+START_DATE = datetime.date(2023, 9, 1)
+END_DATE = datetime.date(2024, 5, 31)
 random.shuffle(SUBJECT_LIST)
 
 
+def get_ids_by_table(table: str):
+    sql = f''' 
+                SELECT id FROM {table};
+            '''
+    result = run_query(sql)
+    id_list = [item[0] for item in result]
+    return id_list
+
+
+def api_key_generator(api_keys):
+    return (key for key in cycle(api_keys))
+
+
+
 def fill_students(n: int):
-    for i in range(1,n):
+    group_ids = get_ids_by_table("groups")
+    api_key_gen = api_key_generator(group_ids)
+
+    for i in range(1, n + 1):
         first_name = fake.first_name()
         second_name = fake.last_name()
+        group_id = next(api_key_gen)
         print(first_name, second_name)
         sql = ''' 
-            INSERT INTO students(first_name,second_name) VALUES(%s,%s);
+            INSERT INTO students(first_name,second_name,group_id) VALUES(%s,%s,%s);
         '''
-        run_query(sql, (first_name, second_name))
+        run_query(sql, (first_name, second_name, group_id))
 
 def fill_groups(n: int):
     for i in range(1,n+1):
@@ -41,17 +63,11 @@ def teachers(n: int):
         run_query(sql, (gender, first_name, second_name))
 
 
-def getteachers_ids():
-    sql = ''' 
-                SELECT id FROM teachers;
-            '''
-    result = run_query(sql)
-    id_list = [item[0] for item in result]
-    return id_list
+
 
 
 def subjects(subject_list: list):
-    teachers_ids = getteachers_ids()
+    teachers_ids = get_ids_by_table("teachers")
     i = 0
     for subject in subject_list:
         if i >= len(subject_list)-1:
@@ -64,9 +80,30 @@ def subjects(subject_list: list):
         '''
         run_query(sql, (subject, teacher_id))
 
+
+def add_marks(blocks: int):
+    students_ids = get_ids_by_table("students")
+    print(students_ids)
+    subjects_ids = get_ids_by_table("subjects")
+    print(subjects_ids)
+
+
+
+    for subject_id in subjects_ids:
+        for block in range(1, blocks+1):
+            date = fake.date_between(start_date=START_DATE, end_date=END_DATE)
+            for student_id in students_ids:
+                mark = random.randint(1, 10)
+                sql = '''
+                            INSERT INTO marks (mark, student_id, subject_id, date) VALUES(%s,%s,%s,%s);
+                        '''
+                run_query(sql, (mark, student_id, subject_id, date))
+
+
 if __name__ == "__main__":
-    # fill_students(5)
-    # fill_groups(3)
-    # teachers(5)
-    # getteachers_ids()
+    fill_groups(3)
+    fill_students(50)
+    teachers(5)
     subjects(SUBJECT_LIST)
+    add_marks(4)
+    pass
