17cc460b40556e0ae50ba6931521dd1fdd996be2
Radzihowski <a.radzihovskiy@gmail.com>
2024-10-17 19:25:11 +0100

    Home work from week 6

--- python_web/06_home_work/query.py
+++ python_web/06_home_work/query.py
@@ -11,6 +11,20 @@ from connect import run_query
 # Знайти список курсів, які відвідує студент.
 # Список курсів, які певному студенту читає певний викладач.
 
+query_1 =  """
+    SELECT AVG(m.mark), s.student_id, s.first_name, s.second_name
+    FROM marks AS m
+    JOIN 
+"""
+
+query_2 =  """
+    SELECT m.student_id, AVG(m.mark)
+    FROM students st
+    JOIN marks as m ON st.id = m.student_id
+    WHERE m.subject_id = 1
+    ORDER BY m.mark DESC
+"""
+
 query_3 =  """
     SELECT AVG(m.mark), g.group_name
     FROM marks AS m
@@ -22,18 +36,52 @@ query_3 =  """
 """
 
 query_4 =  """
+    SELECT AVG(m.mark)
+    FROM marks AS m;
+"""
+
+query_5 =  """
     SELECT *
     FROM subjects AS s
     WHERE s.teacher_id = 1
 """
 
-query_5 =  """
+query_6 =  """
     SELECT *
     FROM students AS s
     WHERE s.group_id = 1
 """
 
+query_7 =  """
+    SELECT m.mark, m.student_id
+    FROM marks AS m
+    JOIN students AS st ON m.student_id = st.id
+    JOIN groups as g ON st.group_id = g.id
+    WHERE m.subject_id = 1 AND st.group_id = 1
+
+
+"""
+
+query_8 =  """
+
+"""
+
+query_9 =  """
+
+"""
+
+query_10 =  """
+
+"""
+
 if __name__ == "__main__":
+    # print(run_query(query_1)) doesn't work
+    # print(run_query(query_2)) doesn't work
     # print(run_query(query_3))
     # print(run_query(query_4))
-    print(run_query(query_5))
+    # print(run_query(query_5))
+    # print(run_query(query_6))
+    print(run_query(query_7))
+    # print(run_query(query_8))
+    # print(run_query(query_9))
+    # print(run_query(query_10))
