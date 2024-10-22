7b88490c71be40e6b8490b7abbb582071a4c1767
Radzihowski <a.radzihovskiy@gmail.com>
2024-10-22 20:47:37 +0100

    Conspect from week 7

---
+++ python_web/module07/day2/EX-09/connect_db.py
@@ -0,0 +1,8 @@
+# Створимо в корені проекту файл підключення до БД
+
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///mynotes.db")
+Session = sessionmaker(bind=engine)
+session = Session()
