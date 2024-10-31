92d19514efeeaa1c217e743ac439a6f5f371f4af
Radzihowski <a.radzihovskiy@gmail.com>
2024-10-31 22:57:51 +0000

    HW7

---
+++ python_web/07_home_work/models.py
@@ -0,0 +1,43 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy.orm import relationship
+
+from .db import Base
+
+
+class Teacher(Base):
+    __tablename__ = "teachers"
+    id = Column(Integer, primary_key=True)
+    fullname = Column(String(120), nullable=False)
+
+
+class Group(Base):
+    __tablename__ = "groups"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+
+
+class Student(Base):
+    __tablename__ = "students"
+    id = Column(Integer, primary_key=True)
+    fullname = Column(String(120), nullable=False)
+    group_id = Column('group_id', ForeignKey('groups.id', ondelete='CASCADE'))
+    group = relationship('Group', backref='students')
+
+
+class Subject(Base):
+    __tablename__ = "subjects"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60), nullable=False)
+    teacher_id = Column('teacher_id', ForeignKey('teachers.id', ondelete='CASCADE'))
+    teacher = relationship('Teacher', backref='disciplines')
+
+
+class Mark(Base):
+    __table__ = 'marks'
+    id = Column(Integer, primary_key=True)
+    grade = Column(Integer, nullable=False)
+    date_of = Column('date_of', Date, nullable=False)
+    student_id = Column('student_id', ForeignKey('teacher.id', ondelete='CASCADE'))
+    subject_id = Column('subject_id', ForeignKey('subjects.id', ondelete='CASCADE'))
+    student = relationship('Student', backup='grade')
+    subject = relationship('Subject', backref='grade')
