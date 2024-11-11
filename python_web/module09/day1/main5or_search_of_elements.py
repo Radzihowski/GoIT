27fcc0f3159ae26a52158f7876ca25a16f259ae5
Radzihowski <a.radzihovskiy@gmail.com>
2024-11-11 22:44:26 +0000

    week 10

--- python_web/module09/day1/main5or_search_of_elements.py
+++ python_web/module09/day1/main5or_search_of_elements.py
@@ -5,5 +5,19 @@ from bs4 import BeautifulSoup
 url = "http://quotes.toscrape.com/"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
+# знайти перший тег <p> на сторінці
 first_paragraph = soup.find("p")
 print(first_paragraph)
+
+# знайти всі теги <p> на сторінці
+all_paragraphs = soup.find_all("p")
+print(all_paragraphs)
+
+# отримати текст першого тега <p> на сторінці
+first_paragraph_text = first_paragraph.get_text()
+print(first_paragraph_text.strip())
+
+# отримати значення атрибута "href" першого тегу <a> на сторінці
+first_link = soup.find("a")
+first_link_href = first_link["href"]
+print(first_link_href)
