94271a117435c58c53bc5f6f3fb0d90d49812a5f
Radzihowski <a.radzihovskiy@gmail.com>
2024-11-12 07:42:47 +0000

    week 10

--- python_web/module09/day1/main5or_search_of_elements.py
+++ python_web/module09/day1/main5or_search_of_elements.py
@@ -21,3 +21,25 @@ print(first_paragraph_text.strip())
 first_link = soup.find("a")
 first_link_href = first_link["href"]
 print(first_link_href)
+
+# Щоб отримати всі дочірні елементи першого тегу <p> на сторінці, використовуємо атрибут children
+body_children = list(first_paragraph.children)
+print(body_children)
+# ['\n', <a href="/login">Login</a>, '\n']
+
+# знайти перший тег <a> всередині першого тегу <div> на сторінці
+first_div = soup.find("div")
+first_div_link = first_div.find("a")
+print(first_div_link)
+
+# Батьківські елементи
+# Щоб отримати батьківський елемент першого тегу <p> на сторінці, ми можемо використовувати властивість parent
+first_paragraph_parent = first_paragraph.parent
+print(first_paragraph_parent)
+
+# Також можна використовувати методи find_parent і find_parents для пошуку батьківських елементів:
+container = soup.find("div", attrs={"class": "quote"}).find_parent("div", class_="col-md-8")
+print(container)
+
+
+
