d9fe69cb3199a1c716cbbb6aef12fc75d26a62fb
Radzihowski <a.radzihovskiy@gmail.com>
2024-11-28 21:42:22 +0000

    week 10 Django

--- python_web/module10/day1/mysite/polls/admin.py
+++ python_web/module10/day1/mysite/polls/admin.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 
-from .models import Question
-
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
+
+admin.site.register(Question)
+admin.site.register(Choice)
