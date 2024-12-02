32dc222876428f3014cc559920e4e549e314b456
Radzihowski <a.radzihovskiy@gmail.com>
2024-12-02 21:55:42 +0000

    week 10 Django

--- python_web/module10/day1/mysite/polls/views.py
+++ python_web/module10/day1/mysite/polls/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
 
-# Create your views here.
-def index(reques):
-    return HttpResponse("Hello, world. You are the polls index.")
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
