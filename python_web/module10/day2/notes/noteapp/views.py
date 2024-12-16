eec41631a48092c1b5e5ca37bcba23917ac74c0c
Radzihowski <a.radzihovskiy@gmail.com>
2024-12-16 21:50:28 +0000

    week 10 Django

--- python_web/module10/day2/notes/noteapp/views.py
+++ python_web/module10/day2/notes/noteapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
-from .forms import TagForm
+from .forms import TagForm, NoteForm
+from .models import Tag
 
 
 # Create your views here.
@@ -19,3 +20,20 @@ def tag(request):
     return render(request, 'noteapp/tag.html', {'form': TagForm()})
 
 def note(request):
+    tags = Tag.objects.all()
+
+    if request.methood == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            new_note = form.save()
+
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choice_tags.iterator():
+                new_note.tags.add(tag)
+
+            return redirect(to='noteapp:main')
+        else:
+            return render(request, 'noteapp/note.html', {"tags": tags, 'form': form})
+    return render(request, 'noteapp/note.html', {"tags": tags, 'form': NoteForm()})
+
+
