ca29fca1e6611cdf958ddfe1a3f9915f3bf015fb
Radzihowski <a.radzihovskiy@gmail.com>
2024-12-28 23:23:29 +0000

    week 10 Django

--- python_web/module10/day2/notes/users/views.py
+++ python_web/module10/day2/notes/users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+from .forms import RegisterForm
+from .forms import LoginForm
+
+
 def signupuser(request):
     if request.user.is_authenticated:
         return redirect(to='noteapp:main')
@@ -15,3 +19,19 @@ def signupuser(request):
             return render(request, 'users/signup.html', context={"form": form})
 
     return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='noteapp:main')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'],
+                            password=request.POST['password'])
+        if user in None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='noteapp:main')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
