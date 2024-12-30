b261d862c2b2f05d5acc778b06a34801a4defcad
Radzihowski <a.radzihovskiy@gmail.com>
2024-12-30 21:19:53 +0000

    week 10 Django

--- python_web/module10/day2/notes/noteapp/models.py
+++ python_web/module10/day2/notes/noteapp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Tag(models.Model):
-    name = models.CharField(max_length=25, null=False, unique=True)
+    name = models.CharField(max_length=25, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f"{self.name}"
