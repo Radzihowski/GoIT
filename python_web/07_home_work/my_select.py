97934ce69fc5d7aa6337326142974a98f9182173
Radzihowski <a.radzihovskiy@gmail.com>
2025-01-24 21:07:50 +0000

    1) Django week 10 2) Home Work week 7

--- python_web/07_home_work/my_select.py
+++ python_web/07_home_work/my_select.py
@@ -1,16 +1,69 @@
+from pprint import pprint
+
+from sqlalchemy import and_, func, desc
 from sqlalchemy import select
+
 from db import session
 from models import Student, Teacher, Subject, Group, Mark
-from sqlalchemy import and_, or_, not_, func
-from pprint import pprint
+
+
 def select_1():
-    ...
-def select_2():
-    ...
-def select_3():
-    ...
+    """Знайти 5 студентів із найбільшим середнім балом з усіх предметів."""
+    query = (
+        select(
+            Student.fullname,
+            func.round(func.avg(Mark.grade), 2).label("Avg_marks")
+        )
+        .join(Mark)
+        .group_by(Student.id)
+        .order_by(desc(func.avg(Mark.grade)))
+        .limit(5)
+    )
+    result = session.execute(query).all()
+    return result
+
+def select_2(subject_id:int):
+    """Знайти студента із найвищим середнім балом з певного предмета."""
+    query = (
+        select(
+            Student.fullname,
+            func.round(func.avg(Mark.grade), 2).label("Avg_marks")
+        )
+        .join(Student)
+        .join(Subject)
+        .where(Subject.id == subject_id)
+        .group_by(Student.id)
+        .order_by(desc(func.avg(Mark.grade)))
+        .limit(1))
+    result = session.execute(query).all()
+    return [i for i in result]
+
+def select_3(subject_id: int):
+    """Знайти середній бал у групах з певного предмета."""
+    query = (
+        select(
+            Group.name,
+            func.round(func.avg(Mark.grade), 2).label("Avg_marks")
+        )
+        .join(Student, Group.id == Student.group_id)  # Join students with their groups
+        .join(Mark, Student.id == Mark.student_id)    # Join marks with students
+        .join(Subject, Mark.subject_id == Subject.id) # Join subjects with marks
+        .where(Subject.id == subject_id)             # Filter by subject
+        .group_by(Group.id)                          # Group by group
+    )
+    result = session.execute(query).all()
+    return [i for i in result]
+
 def select_4():
-    ...
+    """Знайти середній бал на потоці (по всій таблиці оцінок)."""
+    query = (
+        select(
+            func.round(func.avg(Mark.grade), 2).label("Avg_marks")
+        )
+    )
+    result = session.execute(query).scalars().all()
+    return [i for i in result]
+
 def select_5(teacher_id:int):
     """Знайти які курси читає певний викладач."""
     stmt = select(Subject).join(Teacher).where(Teacher.id == teacher_id)
@@ -26,23 +79,42 @@ def select_6(group_id:int):
 
 def select_7(group_id:int, subject_id:int):
     """Знайти оцінки студентів у окремій групі з певного предмета."""
-    stmt = select(Mark).join(Student).join(Group).join(Subject).where(and_(Group.id == group_id),
-                                                                      (Subject.id == subject_id))
+    stmt = select(Mark).join(Student).join(Group).join(Subject).where(and_(Group.id == group_id,
+                                                                           Subject.id == subject_id))
     result = session.execute(stmt).scalars().all()
     return [[i.student.fullname, i.student.group.name, i.subject.name, i.grade] for i in result]
 
 def select_8(teacher_id:int):
     """Знайти середній бал, який ставить певний викладач зі своїх предметів."""
-    stmt = select(func.avg(Mark.grade).label("Avg mark")).join(Subject).join(Teacher).where(Teacher.id == teacher_id).group_by(Subject.id)
+    stmt = select(func.avg(Mark.grade).label("Avg mark")
+                  ).join(Subject).join(Teacher).where(Teacher.id == teacher_id).group_by(Subject.id)
     result = session.execute(stmt).scalars().all()
     return [i for i in result]
-def select_9():
-    ...
-def select_10():
-    ...
+
+
+def select_9(student_id: int):
+    """Знайти список курсів, які відвідує певний студент."""
+    query = select(Subject).join(Mark).join(Student).where(Student.id == student_id)
+    result = session.execute(query).scalars().all()
+    return list(set([i.name for i in result]))
+
+
+def select_10(student_id, teacher_id):
+    """Список курсів, які певному студенту читає певний викладач."""
+    query = select(Subject).join(Mark).join(Student).where(and_(Student.id == student_id,
+                                                                Subject.teacher_id == teacher_id))
+    result = session.execute(query).scalars().all()
+    return set([i.name for i in result])
+
 
 if __name__ == "__main__":
+    # pprint(select_1())
+    # pprint(select_2(1))
+    # pprint(select_3(1))
+    pprint(select_4())
     # select_5(1)
     # select_6(2)
     # pprint(select_7(1, 1))
-    pprint(select_8(1))
+    # pprint(select_8(1))
+    # pprint(select_9(2))
+    # pprint(select_10(2, 1))
--- python_web/module10/ver1/mysite/polls/views.py
+++ python_web/module10/ver1/mysite/polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+
 from .models import Question, Choice
+
 
 # Create your views here.
 def index(request):
@@ -10,25 +12,27 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplaying the question voting form.
+        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice."
+            'error_message': "You didn't select a choice.",
         })
     else:
-
-
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevent data from being posted twice if
+        # user hits the Back button
+    return HttpResponse(reverse('polls:results', args=(question_id)))
