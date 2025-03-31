f0d547fbd0518a77a95f190a3ca8b70742264186
Radzihowski <a.radzihovskiy@gmail.com>
2025-03-31 23:08:56 +0100

    Added pagiantaion and some styles

--- python_web/10_home_work/v2/hw_project/quotes/views.py
+++ python_web/10_home_work/v2/hw_project/quotes/views.py
@@ -1,10 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
 from .utils import get_mongodb
 
 
-def main(request, page):
+def main(request, page=1):
     db = get_mongodb()
     quotes = db.quotes.find() # find all quotes
-    return render(request, 'quotes/index.html', context={'quotes': quotes} ) #Через контекст прокидуємо квотс в шаблон
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page) # Це скільки буде на сторінці
+    quotes_on_page = paginator.page(page)
+    return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})  # Через контекст прокидуємо квотс в шаблон
