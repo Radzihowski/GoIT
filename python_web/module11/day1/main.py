dfbdbd9d91fa0d970b112f3ecc8cc15dec1ff7e6
Radzihowski <a.radzihovskiy@gmail.com>
2025-07-22 22:35:27 +0100

    Rewrite main

--- python_web/module11/day1/main.py
+++ python_web/module11/day1/main.py
@@ -23,10 +23,17 @@ class NoteModel(BaseModel):
     description: str
     done: bool
 
-@app.post("/notes")
-async def create_note(note: NoteModel, db: Session = Depends(get_db)):
-    new_note = Note(name=note.name, description=note.description, done=note.done)
-    db.add(new_note)
-    db.commit()
-    db.refresh(new_note)
-    return new_note
+@app.get("/notes")
+async def read_notes(skip: int = 0, limit: int = Query(default=10, le=100, ge=10), db: Session = Depends(get_db)):
+    notes = db.query(Note).offset(skip).limit(limit).all()
+    return notes
+
+
+@app.get("/notes/{note_id}")
+async def read_note(note_id: int = Path(description="The ID of the note to get", gt=0, le=10),
+                    db: Session = Depends(get_db)):
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if note is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not found')
+    return note
+
