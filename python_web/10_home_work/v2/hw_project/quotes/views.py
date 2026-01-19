d8015ce961424c5747c9ce9e0cb54f9497b743e8
Oleksandr Radzikhovskyi <A.Radzihovskiy@gmail.com>
2026-01-19 21:50:00 +0000

    - Switched from redis to sql - Updated index.html -  Updated the way how to extract data from DB

--- python_web/10_home_work/v2/hw_project/quotes/views.py
+++ python_web/10_home_work/v2/hw_project/quotes/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from .utils import get_mongodb
-
+from .models import Quote
 
 def main(request, page=1):
-    db = get_mongodb()
-    quotes = db.quotes.find() # find all quotes
+    # db = get_mongodb()
+    # quotes = db.quotes.find() # find all quotes
+    quotes = Quote.objects.all() # Отримуємо всі квоти з бази даних
     per_page = 10
     paginator = Paginator(list(quotes), per_page) # Це скільки буде на сторінці
     quotes_on_page = paginator.page(page)
