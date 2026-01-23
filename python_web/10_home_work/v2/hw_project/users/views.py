8957a3559338148eb0ab6a301ae30c622df87651
Oleksandr Radzikhovskyi <A.Radzihovskiy@gmail.com>
2026-01-23 20:44:44 +0000

    add user registration and login forms with authentication, integrate views and templates, and update navigation in base template

--- python_web/10_home_work/v2/hw_project/users/views.py
+++ python_web/10_home_work/v2/hw_project/users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+...
 
-# Create your views here.
+from .forms import RegisterForm, LoginForm
+
+
+def signupuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='quotes:root')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:root')
+        else:
+            return render(request, 'users/signup.html', context={"form": form})
+
+    return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='quotes:root')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='quotes:root')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
+
