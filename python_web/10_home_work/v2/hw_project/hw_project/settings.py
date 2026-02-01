75f2a6ec0c40c075a2a22872c0269df53de36c67
Oleksandr Radzikhovskyi <A.Radzihovskiy@gmail.com>
2026-02-01 07:41:19 +0000

    1) Implemented a password reset mechanism for a registered user; 2) All environment variables now stored in a .env file and used in the settings.py file.

--- python_web/10_home_work/v2/hw_project/hw_project/settings.py
+++ python_web/10_home_work/v2/hw_project/hw_project/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+p&9((qs^5073ndplki2h!g9tfdkguzgbkkp1@)iz+1-3c&q%y'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +128,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_STARTTLS = False
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- python_web/10_home_work/v2/hw_project/users/urls.py
+++ python_web/10_home_work/v2/hw_project/users/urls.py
@@ -7,4 +7,15 @@ urlpatterns = [
     path('signup/', views.signupuser, name='signup'),
     path('login/', views.loginuser, name='login'),
     path('logout/', views.logoutuser, name='logout'),
+    path('reset-password/', views.ResetPasswordView.as_view(), name='password_reset'),
+    path('reset-password/done/', views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset-password/confirm/<uidb64>/<token>/',
+         views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                          success_url='/users/reset-password/complete/'),
+         name='password_reset_confirm'),
+    path('reset-password/complete/',
+         views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
+
 ]
