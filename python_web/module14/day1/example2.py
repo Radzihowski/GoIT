26fd2aa1aacd079a886255a99bf49160febfa671
Oleksandr Radzikhovskyi <A.Radzihovskiy@gmail.com>
2026-02-18 23:31:13 +0000

    Test fixture

---
+++ python_web/module14/day1/example2.py
@@ -0,0 +1,21 @@
+# У загальному випадку Test fixture - це підготовка, необхідна для виконання
+# тестів і необхідні дії з очищення після виконання тестів. Наприклад,
+# створення тимчасових баз даних перед тестами.
+
+import unittest
+
+class MyTest(unittest.TestCase):
+    def setUp(self):
+        self.my_list = [1, 2, 3]
+
+    def tearDown(self):
+        del self.my_list
+
+    def test_list_length(self):
+        self.assertEqual(len(self.my_list), 3)
+
+    def test_list_contents(self):
+        self.assertListEqual(self.my_list, [1, 2, 3])
+
+if __name__ == '__main__':
+    unittest.main()
